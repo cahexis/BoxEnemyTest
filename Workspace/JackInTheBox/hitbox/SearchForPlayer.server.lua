11bac4b057f736f1ad53a52e8c2577c637fe4eea
Emi <183315760+cahexis@users.noreply.github.com>
2025-04-19 20:40:22 -0500

    Create SearchForPlayer.server.lua

---
+++ Workspace/JackInTheBox/hitbox/SearchForPlayer.server.lua
@@ -0,0 +1,21 @@
+local hitbox = script.Parent
+local anim = Instance.new("Animation")
+local hum = hitbox.Parent.Humanoid
+anim.AnimationId = "rbxassetid://130945730016421"
+local track = hum:LoadAnimation(anim)
+local db = false
+local slamsound = hitbox.slamsound
+
+hitbox.Touched:Connect(function(hit)
+	local player = game:GetService("Players"):GetPlayerFromCharacter(hit.Parent)
+	if db == false then
+		if player then
+			db = true
+			track:Play()
+			wait(0.9)
+			slamsound:Play()
+			wait(5)
+			db = false
+		end
+	end
+end)
