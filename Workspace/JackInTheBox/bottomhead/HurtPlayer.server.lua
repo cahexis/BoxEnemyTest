6906b9729b4e11102756c3b4a7af1a2d63402381
Emi <183315760+cahexis@users.noreply.github.com>
2025-04-19 20:40:51 -0500

    Create HurtPlayer.server.lua

---
+++ Workspace/JackInTheBox/bottomhead/HurtPlayer.server.lua
@@ -0,0 +1,21 @@
+local head = script.Parent
+local anim = Instance.new("Animation")
+local hum = head.Parent.Humanoid
+local db = false
+
+
+
+
+head.Touched:Connect(function(hit)
+	local player = game:GetService("Players"):GetPlayerFromCharacter(hit.Parent)
+	if db == false then
+		if player then
+			db = true
+			local char = player.Character
+			local hum = char:WaitForChild("Humanoid")
+			hum:TakeDamage(100)
+			wait(5)
+			db = false
+		end
+	end
+end)
